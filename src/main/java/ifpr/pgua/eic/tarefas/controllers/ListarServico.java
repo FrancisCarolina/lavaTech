53160660e469bd009d756add487b4037e59838c0
Nikolas <nikolasrailanmp@gmail.com>
2023-11-16 17:17:44 -0300

    adicionando combobox vazia na tela listar servico

--- src/main/java/ifpr/pgua/eic/tarefas/controllers/ListarServico.java
+++ src/main/java/ifpr/pgua/eic/tarefas/controllers/ListarServico.java
@@ -4,6 +4,7 @@ import ifpr.pgua.eic.tarefas.App;
 import ifpr.pgua.eic.tarefas.model.repositories.RepositorioServico;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.input.MouseEvent;
@@ -11,7 +12,6 @@ import javafx.scene.input.MouseEvent;
 public class ListarServico {
 
     private RepositorioServico repositorioServico;
-    
 
     public ListarServico(RepositorioServico repositorioServico) {
         this.repositorioServico = repositorioServico;
@@ -37,6 +37,9 @@ public class ListarServico {
 
     @FXML
     private TableView<?> tbvServicos;
+
+    @FXML
+    private ComboBox<?> cbFiltro;
 
     @FXML
     void agendar(ActionEvent event) {
